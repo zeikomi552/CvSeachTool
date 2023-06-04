4987293bbb3fa3e4081e9f5e3b8e7e972bff1deb
zeikomi552 <premium.tsubu@gmail.com>
2023-06-05 05:29:45 +0900

    https://github.com/zeikomi552/CvSeachTool/issues/13

--- CvSeachTool/Views/BookmarkRenameV.xaml.cs
+++ CvSeachTool/Views/BookmarkRenameV.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace CvSeachTool.Views
     /// <summary>
     /// BookmarkRenameV.xaml の相互作用ロジック
     /// </summary>
-    public partial class BookmarkRenameV : Window
+    public partial class BookmarkRenameV : MetroWindow
     {
         public BookmarkRenameV()
         {
